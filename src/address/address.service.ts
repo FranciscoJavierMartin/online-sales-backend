248e4ff59d5d4b1343f7dfb403eee04af808a2bf
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-09-26 00:25:40 +0200

    Create address

--- src/address/address.service.ts
+++ src/address/address.service.ts
@@ -1,26 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { AddressEntity } from './entities/address.entity';
 import { CreateAddressDto } from './dto/create-address.dto';
-import { UpdateAddressDto } from './dto/update-address.dto';
 
 @Injectable()
 export class AddressService {
-  create(createAddressDto: CreateAddressDto) {
-    return 'This action adds a new address';
-  }
+  constructor(
+    @InjectRepository(AddressEntity)
+    private readonly addressRepository: Repository<AddressEntity>,
+  ) {}
 
-  findAll() {
-    return `This action returns all address`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} address`;
-  }
-
-  update(id: number, updateAddressDto: UpdateAddressDto) {
-    return `This action updates a #${id} address`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} address`;
+  async create(
+    createAddressDto: CreateAddressDto,
+    userId: number,
+  ): Promise<AddressEntity> {
+    return this.addressRepository.save({
+      ...createAddressDto,
+      userId,
+    });
   }
 }
--- src/address/dto/create-address.dto.ts
+++ src/address/dto/create-address.dto.ts
@@ -1,1 +1,21 @@
-export class CreateAddressDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsInt, IsOptional, IsString } from 'class-validator';
+
+export class CreateAddressDto {
+  @ApiProperty({ name: 'complement' })
+  @IsString()
+  @IsOptional()
+  complement: string;
+
+  @ApiProperty({ name: 'numberAddress' })
+  @IsInt()
+  numberAddress: number;
+
+  @ApiProperty({ name: 'cep' })
+  @IsString()
+  cep: string;
+
+  @ApiProperty({ name: 'cityId' })
+  @IsInt()
+  cityId: number;
+}
