a2032a349ff0b6adb5128e0a662a9cc690d5bd74
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-09-28 22:06:18 +0200

    Add property to user

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,19 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({ name: 'name' })
   @IsString()
   name: string;
 
+  @ApiProperty({ name: 'email' })
   @IsString()
   @IsEmail()
   email: string;
 
+  @ApiProperty({ name: 'phone' })
   @IsString()
   phone: string;
 
+  @ApiProperty({ name: 'cpf' })
   @IsString()
   cpf: string;
 
+  @ApiProperty({ name: 'password' })
   @IsString()
   password: string;
 }
